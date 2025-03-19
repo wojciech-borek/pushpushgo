dd885680244f068f798e6fddbfd4144a3933c5bc
wojbor90 <wojbor90@gmail.com>
2025-03-19 11:02:44 +0100

    init

---
+++ api/sw.js
@@ -0,0 +1,7 @@
+/**
+ * Warning: Before you edit this file or merge with other current worker
+ * In case of edit or rename, merge with other: be careful existing projects with subscribers registrations on this worker path, can be destroyed after changes and you may occur some issues with deliverability
+ * In case of two workers: be sure that you provide other file name and update project in PushPushGo Dashboard and use /ppg scope.
+ * If you have any questions before you take any action please contact with support@pushpushgo.com for consulting service worker change strategy
+ */
+importScripts('https://s-eu-1.pushpushgo.com/67da9066617101f499c2a1c1/worker.js');
