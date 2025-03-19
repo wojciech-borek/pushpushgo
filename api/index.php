4d8a54c137dc827005809c26fd8976ac0af0ac09
wojbor90 <wojbor90@gmail.com>
2025-03-19 11:06:35 +0100

    pushpush integration

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Integracja PushPushGo</title>
-    <script charset="UTF-8" src="https://s-eu-1.pushpushgo.com/js/67da9369ad12b66d0dee372c.js" async="async"></script>
+    <script charset="UTF-8" src="https://s-eu-1.pushpushgo.com/js/67da96e65957ad72be158dca.js" async="async"></script>
 </head>
 <body>
 <h1>Integracja z PushPushGo</h1>
--- api/sw.js
+++ api/sw.js
@@ -4,4 +4,4 @@
  * In case of two workers: be sure that you provide other file name and update project in PushPushGo Dashboard and use /ppg scope.
  * If you have any questions before you take any action please contact with support@pushpushgo.com for consulting service worker change strategy
  */
-importScripts('https://s-eu-1.pushpushgo.com/67da9066617101f499c2a1c1/worker.js');
+importScripts('https://s-eu-1.pushpushgo.com/67da96e65957ad72be158dca/worker.js');
