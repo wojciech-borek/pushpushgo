608d265cf4a0b4da0576e8cf8ddc7fe468b0e432
wojbor90 <wojbor90@gmail.com>
2025-03-19 11:03:06 +0100

    init

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integracja PushPushGo</title>
+    <script charset="UTF-8" src="https://s-eu-1.pushpushgo.com/js/67da9369ad12b66d0dee372c.js" async="async"></script>
+</head>
+<body>
+<h1>Integracja z PushPushGo</h1>
+</body>
+</html>
